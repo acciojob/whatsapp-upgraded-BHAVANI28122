58540da3fb0787e157c3b98e1cee552ef4310889
BHAVANI28122 <108610488+BHAVANI28122@users.noreply.github.com>
2023-01-29 17:51:16 +0530

    do

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -10,7 +10,7 @@ public class Message {
     public Message(int id, String content, Date timestamp) {
         this.id = id;
         this.content = content;
-        this.timestamp = new Date();
+        this.timestamp = timestamp;
     }
     public Message(){
 
